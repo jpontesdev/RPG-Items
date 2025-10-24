159984bc2044c4b89c97fba0680de53d6fae236a
Gabriel Rainwalker <gduartemarques@gmail.com>
2025-10-23 22:05:27 -0300

    Atualizar assets

--- obj/Debug/net9.0/RPGItemnsAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/RPGItemnsAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGItemnsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+636f273fd4575816400b1a7647e69608f74caefe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d61d793e761f3c26017cf329d52f96645ea7688")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGItemnsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGItemnsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
